12a5c391862f341192dd7214f342ada124e817ef
David Kaufmann <david@kaufmann.dev>
2022-11-09 09:32:35 +0100

    refactor(bookmark): simplify bookmark button logic and template

--- resources/js/Shared/BookmarkButton.svelte
+++ resources/js/Shared/BookmarkButton.svelte
@@ -7,66 +7,24 @@
 
     let handleBookmark = () => {
         if(isChecked){
-            //axios.post('/bookmarks/delete/' + checkId)
-            //.then((x)=>{
-            //    router.reload();
-            //});
-            router.post(`/bookmarks/delete/${checkId}`, {
-                onFinish: visit => {
-                    if (visit) {
-                        router.reload();
-                    }
-                },
-            });
+            router.post(`/bookmarks/delete/${checkId}`);
         } else {
-            //axios.post('/bookmarks/' + checkId)
-            //.then((x)=>{
-            //    router.reload();
-            //});
-            router.post(`/bookmarks/${checkId}`, {
-                onBefore: (visit) => {
-
-                },
-                onStart: (visit) => {
-
-                },
-                onProgress: (progress) => {
-
-                },
-                onSuccess: (page) => {
-
-                },
-                onError: (errors) => {
-
-                },
-                onCancel: () => {
-
-                },
-                onFinish: visit => {
-                    router.reload();
-                },
-            });
+            router.post(`/bookmarks/${checkId}`);
         }
+        isChecked = !isChecked
     }
-
-    let isHovering = false;
 </script>
 
 {#if $page.props.auth.user}
-        <button on:click={handleBookmark} class="tw-py-0.5 tw-px-3 tw-rounded-3xl tw-bg-yellow-400 tw-text-black" on:click={() => {isChecked = !isChecked}}
-            on:mouseover={() => isHovering = true}
-                    on:mouseout={() => isHovering = false}>
-            {#if isChecked}
-                <i
-                    class:hover={isHovering ? 'bi bi-bookmark-fill' : ''}
-                    class={isHovering ? 'bi bi-bookmark' : 'bi bi-bookmark-fill'}
-                    
-                ></i>
-            {:else}
-                <i
-                    class:hover={isHovering ? 'bi bi-bookmark' : ''}
-                    class={isHovering ? 'bi bi-bookmark-fill' : 'bi bi-bookmark'}
-                ></i>
-            {/if}
-        </button>
+    <button
+        on:click={handleBookmark}
+        class="tw-py-0.5 tw-px-3 tw-rounded-3xl tw-bg-yellow-400 tw-text-black"
+        type="button"
+    >
+        {#if isChecked}
+            <i class="bi bi-bookmark-fill"></i>
+        {:else}
+            <i class="bi-bookmark"></i>
+        {/if}
+    </button>
 {/if}
