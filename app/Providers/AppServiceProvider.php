daab02567f1233bb0f33fc14133beddf2d35abea
David Kaufmann <david@kaufmann.dev>
2023-01-30 10:10:21 +0100

    refactor(app): remove force https scheme from service provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,6 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -25,6 +24,5 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
-        URL::forceScheme('https');
     }
 }
