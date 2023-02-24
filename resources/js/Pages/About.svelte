e8e4b4c950120ba4f4d037fd3ae76474565ab337
David Kaufmann <david@kaufmann.dev>
2023-02-24 19:09:54 +0100

    docs(about): update project timeline and platform description

--- resources/js/Pages/About.svelte
+++ resources/js/Pages/About.svelte
@@ -12,19 +12,19 @@
             Herzlich willkommen bei "Jung in Krems". Diese Webseite wurde im Rahmen der gemeinsamen Diplomarbeit von David Kaufmann und Johannes Spindelberger an der HTL Krems entwickelt. Während David Kaufmann für die Erstellung der Webseite verantwortlich war, beschäftige sich Johannes Spindelberger mit der Entwicklung einer Mobilapplikation.
         </div>
         <div class="tw-mb-4">
-            Das Projekt wurde am 29. September 2022 ins Leben gerufen, als sich die Teammitglieder erstmals mit der Stadtverwaltung trafen, um die Grundlagen zu legen. Durch enge Zusammenarbeit entstand "Jung in Krems" – eine Plattform, die jungen Menschen in Krems Unterstützung bietet und ihnen bei der Orientierung in der Stadt hilft.
+            Dieses Projekt wurde mit dem 29. September 2022 - beim ersten Zusammentreffen sämtlicher Projektteammitglieder mit der Stadtverwaltung - gestartet: an diesem Tag wurden die Grundlagen und Meilensteine geplant und besprochen. Durch diese weitergehende enge Zusammenarbeit entstand "Jung in Krems" – eine Plattform, die jungen Menschen in Krems Unterstützung bietet und ihnen bei der Orientierung in der Stadt hilft.
         </div>
         <div class="tw-mb-4">
-            Daraufhin folgte eine monatelange Entwicklungsphase, in der die Webseite erstellt wurde. Die technische Realisierung der Webseite wurde durch den Einsatz moderner Technologien wie Laravel 9, Laravel Sanctum, Svelte, Inertia.js, MySQL, Tailwind und Bootstrap ermöglicht. Dadurch entstand eine benutzerfreundliche Webapplikation, die Informationen leicht zugänglich macht und zugleich ästhetisch ansprechend ist. Das Design und die Programmlogik der Webseite wurden eigenständig von David Kaufmann entwickelt.
+            Daraufhin folgte eine monatelange Entwicklungsphase, in der die Webseite zuerst konzipiert und anschließlich durch den Einsatz moderner Technologien wie Laravel 9, Laravel Sanctum, Svelte, Inertia.js, MySQL, Tailwind und Bootstrap realisiert wurde. Dadurch entstand eine benutzerfreundliche Webapplikation, die Informationen leicht zugänglich macht und zugleich ästhetisch ansprechend ist. Das Design und die Programmlogik der Webseite wurden eigenständig von David Kaufmann entwickelt.
         </div>
         <div class="tw-mb-4">
-            Am 19. April des folgenden Jahres präsentierten die Teammitglieder das Projekt der Stadtverwaltung und Mitgliedern des Gemeinderates. Nach diesem wichtigen Meilenstein wurden Rückmeldungen aufgenommen und letzte Anpassungen vorgenommen, um sicherzustellen, dass die Plattform optimal den Bedürfnissen der Zielgruppe entspricht.
+            Am 19. April des folgenden Jahres stellten die Teammitglieder das Projekt der Stadtverwaltung und Mitgliedern des Gemeinderates vor. Nach diesem wichtigen Meilenstein wurden essentielle Rückmeldungen und Feedback auf-, und letzte Adaptierungen vorgenommen, um sicherzustellen, dass die Plattform optimal den Bedürfnissen der Zielgruppe der Jugendlichen und jungen Erwachsenen entspricht.
         </div>
         <div class="tw-mb-4">
-            Schließlich wurde das Projekt am 9. August offiziell veröffentlicht. Diese wichtige Phase markierte den Anfang einer neuen Möglichkeit für junge Menschen in Krems, die nun eine Quelle haben, um sich über lokale Veranstaltungen und Bildungsangebote zu informieren.
+            Schließlich wurde das Projekt am 9. August offiziell veröffentlicht. Dieser letzte Meilenstein markiert den Anfang einer neuen digitalen Plattform für junge Menschen in Krems, welche eine geeignete Quelle für lokale Veranstaltungen und Bildungsangebote darbietet.
         </div>
         <div class="tw-mb-4">
-            Vielen Dank für Ihr Interesse an "Jung in Krems". Wir laden Sie herzlich ein, unsere Webseite zu erkunden und von den bereitgestellten Informationen zu profitieren.
+            Vielen Dank für Ihr Interesse an "Jung in Krems"! Wir laden Sie herzlich ein, unsere Webseite zu erkunden und von den bereitgestellten Informationen zu profitieren. Krems wartet auf Sie!
         </div>
     </div>
 </Layout>
