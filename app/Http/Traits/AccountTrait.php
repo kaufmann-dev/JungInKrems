2339ef82bee130b7095192dd405c8a981f1bfbb8
David Kaufmann <david@kaufmann.dev>
2023-03-18 21:01:52 +0100

    change password trait

--- app/Http/Traits/AccountTrait.php
+++ app/Http/Traits/AccountTrait.php
@@ -100,25 +100,4 @@ trait AccountTrait
             'terms.accepted' => 'Die Nutzungsbedingungen müssen akzeptiert werden.'
         ]);
     }
-
-    public function validatePasswordReset(Request $request)
-    {
-        $this->validate($request, [
-            'OLD_PASSWORD' => 'required|current_password',
-            'PASSWORD' => 'required|string|min:8|max:255|regex:/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).+$/',
-            'PASSWORD_CONFIRMATION' => 'required|same:PASSWORD',
-        ], [
-            'OLD_PASSWORD.required' => 'Das alte Passwort ist erforderlich.',
-            'OLD_PASSWORD.current_password' => 'Das alte Passwort ist falsch.',
-
-            'PASSWORD.required' => 'Das neue Passwort ist erforderlich.',
-            'PASSWORD.string' => 'Das neue Passwort muss ein String sein.',
-            'PASSWORD.min' => 'Das neue Passwort muss mindestens 8 Zeichen lang sein.',
-            'PASSWORD.max' => 'Das neue Passwort darf maximal 255 Zeichen lang sein.',
-            'PASSWORD.regex' => 'Das neue Passwort muss mindestens einen Großbuchstaben, einen Kleinbuchstaben und eine Zahl enthalten.',
-
-            'PASSWORD_CONFIRMATION.required' => 'Die Passwortbestätigung ist erforderlich.',
-            'PASSWORD_CONFIRMATION.same' => 'Die Passwörter stimmen nicht überein.',
-        ]);
-    }
 }
